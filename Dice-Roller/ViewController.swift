828e1e0309e1a1fe30631c5194926df19003fe90
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-08-19 22:16:00 +0100

    layout and outlets

---
+++ Dice-Roller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dice-Roller
+//
+//  Created by Jess Leivers on 19/08/2017.
+//  Copyright © 2017 Sarah Jessica London. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceLeft: UIImageView!
+    @IBOutlet weak var diceRight: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollBtnPressed(_ sender: Any) {
+        
+    }
+
+}
+
